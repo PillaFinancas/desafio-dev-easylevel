7d2362bdfc4d6cba328643a4f9ef2c1e427c62d1
Bruno Martins <bruno.martins@esx.com.br>
2021-12-20 15:18:35 -0300

    Initial Commit

---
+++ ReajusteSalarial/ReajusteSalarial/Dominio/Salario.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ReajusteSalarial.Dominio
+{
+    public class Salario
+    {
+        public double Valor { get; set; }
+    }
+}
---
+++ ReajusteSalarial/ReajusteSalarial/Servico/CalcularReajuste.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ReajusteSalarial.Servico
+{
+    public class CalcularReajuste
+    {
+        public static double  Calcular(double salario)
+        {
+            if (salario >= 0 && salario <= 1999.99)
+            {
+                return (salario * 0.2) + salario;
+            }
+            else if (salario >= 2000 && salario <= 3999.99)
+            {
+                return (salario * 0.15) + salario;
+            }
+            else if (salario >= 4000 && salario <= 6999.99)
+            {
+                return (salario * 0.10) + salario;
+            }
+            else
+            {
+                return (salario * 0.5) + salario;
+            }
+        }
+    }
+}
